c11947b8b15d019fee8c320a3f08a935a016d686
shepherdp <shepherd.berea@gmail.com>
2024-07-07 00:50:42 -0400

    LOTS of changes

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const el1 = document.getElementById('container1');
-const hiddenDiv1 = document.getElementById('hidden-div1');
-el1.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv1.style.display = 'block';
-  // hiddenDiv.style.visibility = 'visible';
-});
+//const el1 = document.getElementById('container1');
+//if (el1) {
+//  const hiddenDiv1 = document.getElementById('hidden-div1');
+//  el1.addEventListener('mouseover', function handleMouseOver() {
+//    hiddenDiv1.style.display = 'block';
+//  // hiddenDiv.style.visibility = 'visible';
+//  });
+//
+//  el1.addEventListener('mouseout', function handleMouseOut() {
+//    hiddenDiv1.style.display = 'none';
+//  // hiddenDiv.style.visibility = 'hidden';
+//  });
+//}
 
-el1.addEventListener('mouseout', function handleMouseOut() {
-  hiddenDiv1.style.display = 'none';
-  // hiddenDiv.style.visibility = 'hidden';
-});
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-const el2 = document.getElementById('container2');
-const hiddenDiv2 = document.getElementById('hidden-div2');
-el2.addEventListener('mouseover', function handleMouseOver() {
-  hiddenDiv2.style.display = 'block';
-  // hiddenDiv.style.visibility = 'visible';
-});
-
-el2.addEventListener('mouseout', function handleMouseOut() {
-  hiddenDiv2.style.display = 'none';
-  // hiddenDiv.style.visibility = 'hidden';
-});
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
